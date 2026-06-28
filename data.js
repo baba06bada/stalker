9232717f1f5995adb60f077df5e7d0f28cdba1e0
baba06bada <emnozkn70@gmail.com>
2026-06-28 14:55:45 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+// == PORTAL BILGILERIN (kendininkilerle degistir) ==
+var TOKEN = "***";
+var PORTAL_BASE = "***";
+var PORTAL_HOST = "***";
+var MAC_ADDR = "***";
+
+// == KANAL LISTESI (data.js'den alinan) ==
+var CHANNELS = [];
+
+// Eger statik kanal kullanmak istemiyorsan (portal baglantisi ile),
+// CHANNELS'i bos birak. Portal URL + MAC ile otomatik yuklenir.
